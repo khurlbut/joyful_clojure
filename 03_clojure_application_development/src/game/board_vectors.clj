1e9c15c21cc46a3414435c0b51534c167b55aa54
Kevin Hurlbut <Ke015t7@l-sfh9v2lvcg-m.gapinc.gap.com>
2019-12-26 18:12:58 -0800

    further refince vectoring methods.

--- 03_clojure_application_development/src/game/board_vectors.clj
+++ 03_clojure_application_development/src/game/board_vectors.clj
@@ -20,34 +20,34 @@
     (let [l (if (> l (count v)) (count v) l)]
     (subvec v 0 l)))
 
+(defn- get-vector
+  ([f s] (head-subvec f s))
+  ([f s l] (tail-subvec (head-subvec f s) l)))
+
+(defn- get-reverse-vector
+  ([f s] (head-subvec f s reverse-vector))
+  ([f s l] (tail-subvec (head-subvec f s reverse-vector) l)))
+
 (defn get-vector-east
-  ([s] (head-subvec get-row s))
-  ([s l] (tail-subvec (head-subvec get-row s) l)))
+  [s & l] (apply get-vector get-row s l))
 
 (defn get-vector-west
-  ([s] (head-subvec get-row s reverse-vector))
-  ([s l] (tail-subvec (head-subvec get-row s reverse-vector) l)))
+  [s & l] (apply get-reverse-vector get-row s l))
 
 (defn get-vector-north
-  ([s] (head-subvec get-col s))
-  ([s l] (tail-subvec (head-subvec get-col s) l)))
+  [s & l] (apply get-vector get-col s l))
 
 (defn get-vector-south
-  ([s] (head-subvec get-col s reverse-vector))
-  ([s l] (tail-subvec (head-subvec get-col s reverse-vector) l)))
+  [s & l] (apply get-reverse-vector get-col s l))
 
 (defn get-vector-north-east
-  ([s] (head-subvec get-south-west-diagonal s reverse-vector))
-  ([s l] (tail-subvec (head-subvec get-south-west-diagonal s reverse-vector) l)))
+  [s & l] (apply get-reverse-vector get-south-west-diagonal s l))
 
 (defn get-vector-north-west
-  ([s] (head-subvec get-south-east-diagonal s reverse-vector))
-  ([s l] (tail-subvec (head-subvec get-south-east-diagonal s reverse-vector) l)))
+  [s & l] (apply get-reverse-vector get-south-east-diagonal s l))
 
 (defn get-vector-south-east
-  ([s] (head-subvec get-south-east-diagonal s))
-  ([s l] (tail-subvec (head-subvec get-south-east-diagonal s) l)))
+  [s & l] (apply get-vector get-south-east-diagonal s l))
 
 (defn get-vector-south-west
-  ([s] (head-subvec get-south-west-diagonal s))
-  ([s l] (tail-subvec (head-subvec get-south-west-diagonal s) l)))
+  [s & l] (apply get-vector get-south-west-diagonal s l))
