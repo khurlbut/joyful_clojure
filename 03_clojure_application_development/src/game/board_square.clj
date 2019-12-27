ea9c6004372d342fdfa2394ef94166e31de17663
Kevin Hurlbut <Ke015t7@l-sfh9v2lvcg-m.gapinc.gap.com>
2019-12-26 21:47:42 -0800

    add skeleton for board-square.

---
+++ 03_clojure_application_development/src/game/board_square.clj
@@ -0,0 +1,4 @@
+(ns game.board-square
+  (:require [game.board-vectors :refer :all]))
+
+(defn square [] (println "board-squarei!"))
---
+++ 03_clojure_application_development/tests/game/board_square_test.clj
@@ -0,0 +1,7 @@
+(ns game.board-square-test
+  (:require [clojure.test :refer :all]
+            [game.board-square :refer :all]))
+
+(deftest test-square
+  (testing "should invoke board square"
+    (square)))
