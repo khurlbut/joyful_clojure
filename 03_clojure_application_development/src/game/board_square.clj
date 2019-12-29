96835cf3bddebbc5bbe0242eaec8eb4eb72e34d5
Kevin Hurlbut <Ke015t7@l-sfh9v2lvcg-m.gapinc.gap.com>
2019-12-28 21:08:18 -0800

    add identity to square.

--- 03_clojure_application_development/src/game/board_square.clj
+++ 03_clojure_application_development/src/game/board_square.clj
@@ -3,14 +3,16 @@
             [game.board-vectors :refer :all]))
 
 (defn square [s]
-  (let [directions-map {}]
-  (assoc directions-map
-    :north      (get-vector-north      s)
-    :north-east (get-vector-north-east s)
-    :east       (get-vector-east       s)
-    :south-east (get-vector-south-east s)
-    :south      (get-vector-south      s)
-    :south-west (get-vector-south-west s)
-    :west       (get-vector-west       s)
-    :north-west (get-vector-north-west s)
-  )))
+  (let [sq {
+          :id s
+          :dirs {
+            :north      (get-vector-north      s)
+            :north-east (get-vector-north-east s)
+            :east       (get-vector-east       s)
+            :south-east (get-vector-south-east s)
+            :south      (get-vector-south      s)
+            :south-west (get-vector-south-west s)
+            :west       (get-vector-west       s)
+            :north-west (get-vector-north-west s)
+            }}]
+        sq))
