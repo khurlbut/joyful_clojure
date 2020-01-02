30ba489a8b7ab512e900c67af4f2331c37ef36ff
Kevin Hurlbut <Ke015t7@l-sfh9v2lvcg-m.gapinc.gap.com>
2020-01-02 12:54:00 -0800

    add bishop action

--- 03_clojure_application_development/src/game/chess/actions.clj
+++ 03_clojure_application_development/src/game/chess/actions.clj
@@ -9,3 +9,10 @@
         east  (build-action-map (get-vector s :east)  c b)
         west  (build-action-map (get-vector s :west)  c b)]
   (reduce merge-maps [north south east west])))
+
+(defn bishop-actions [s c b]
+  (let [north-east (build-action-map (get-vector s :north-east) c b)
+        north-west (build-action-map (get-vector s :north-west) c b)
+        south-east (build-action-map (get-vector s :south-east) c b)
+        south-west (build-action-map (get-vector s :south-west) c b)]
+  (reduce merge-maps [north-east north-west south-east south-west])))
